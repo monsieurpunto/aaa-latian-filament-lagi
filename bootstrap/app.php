15e8fc066a494ae2830e9748fcd179b8befa83a3
monsieurpunto <puntoriza2@gmail.com>
2026-07-15 19:32:54 +0700

    Add middleware to redirect guests to the login route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo(
+            fn (Request $request) => route('filament.auth.auth.login'),
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
